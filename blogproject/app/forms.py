cfdbc8cf8d765511f26fb9db2ad91de069aef804
aarun2786 <150610612+aarun2786@users.noreply.github.com>
2024-12-12 23:22:58 +0530

    add placehoder to title field

--- blogproject/app/forms.py
+++ blogproject/app/forms.py
@@ -9,8 +9,8 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['tag' ,'title', 'content']#
         widgets ={
-            'title':forms.TextInput(attrs={'class':'form-control'}),
-            'tag':forms.SelectMultiple(attrs={'class':"form-select"},choices=[(tag.tag_name,tag.id)  for tag in Tag.objects.all()]),
+            'title':forms.TextInput(attrs={'class':'form-control mb-2','placeholder':"Title"}),
+            'tag':forms.SelectMultiple(attrs={'class':"form-select mb-2"},choices=[(tag.tag_name,tag.id)  for tag in Tag.objects.all()]),
             'content':forms.Textarea(attrs={"class":"tt"})
         }
-
+        label  ={'tag':""}
